5df1f1f4fcf8b487346abd14f6e75cd546ee8f84
Chamster <89588942+Chamsterr@users.noreply.github.com>
2022-03-06 14:11:48 +0300

    bboard urls.py

--- samplesite/urls.py
+++ samplesite/urls.py
@@ -1,9 +1,6 @@
 from django.contrib import admin
 from django.urls import path
 
-from bboard.views import index
-
 urlpatterns = [
-    path('bboard/', index),
     path('admin/', admin.site.urls),
 ]
